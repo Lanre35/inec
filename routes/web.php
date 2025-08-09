c3b3b5252814263bfbb0c29818c445eef3be94ff
lanre36 <lanreazeez99@yahoo.com>
2025-08-09 16:29:36 +0100

    middleware

--- routes/web.php
+++ routes/web.php
@@ -15,24 +15,24 @@ Route::resource('users', UsersController::class);
 
 // Logout
 
-Route::get('/logout', [UsersController::class,'logout'])->name('logout');
+Route::get('/logout', [UsersController::class,'logout'])->name('logout')->middleware('auth');
 
 
-Route::view('/dashboard', 'dashboard')->name('dashboard');
-Route::get('/polling-unit',[PollingUnitResultController::class,'index']);
-Route::get('/search', [PollingUnitResultController::class, 'show']);
+Route::view('/dashboard', 'dashboard')->name('dashboard')->middleware('auth');
+Route::get('/polling-unit',[PollingUnitResultController::class,'index'])->middleware('auth');
+Route::get('/search', [PollingUnitResultController::class, 'show'])->middleware('auth');
 
-Route::get('/total-result-polling-unit', [TotalResultPollUnitController::class, 'index'])->name('polling-result');
-Route::get('/total', [TotalResultPollUnitController::class, 'show'])->name('total');
-
-
-Route::resource('create-polling-unit', CreateNewPollUnitController::class);
+Route::get('/total-result-polling-unit', [TotalResultPollUnitController::class, 'index'])->name('polling-result')->middleware('auth');
+Route::get('/total', [TotalResultPollUnitController::class, 'show'])->name('total')->middleware('auth');
 
 
-Route::get('dd',[CreateNewPollUnitController::class,'getWard']);
-Route::get('pol',[CreateNewPollUnitController::class,'getPollingUnit']);
-Route::get('ss',[PollingUnitResultController::class,'show']);
+Route::resource('create-polling-unit', CreateNewPollUnitController::class)->middleware('auth');
 
 
-Route::get('show', [PollingUnitResultController::class, 'show'])->name('show');
+Route::get('dd',[CreateNewPollUnitController::class,'getWard'])->middleware('auth');
+Route::get('pol',[CreateNewPollUnitController::class,'getPollingUnit'])->middleware('auth');
+Route::get('ss',[PollingUnitResultController::class,'show'])->middleware('auth');
+
+
+Route::get('show', [PollingUnitResultController::class, 'show'])->name('show')->middleware('auth');
 
